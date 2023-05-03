b2fd5c79c5c8e64a0ea4f86be22fbe97744bbbe1
choi-moon <56420577+choi-moon@users.noreply.github.com>
2023-05-03 19:44:19 +0900

    Delete js directory

--- js/sidebox.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var currentPosition = parseInt($(".quickmenu").css("top"));
-    $(window).scroll(function() {
-      var position = $(window).scrollTop(); 
-      $(".quickmenu").stop().animate({"top":position+currentPosition+"px"},1000);
-    });
-  });
